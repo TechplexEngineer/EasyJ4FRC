5a6f6f8406f36feadf01f2abfd34321872046057
Blake Bourque <Techplex.Engineer@gmail.com>
2014-05-23 19:52:59 -0400

    Adding digital output blocks

--- apps/robotbuilder/blocks/digital.js
+++ apps/robotbuilder/blocks/digital.js
@@ -43,7 +43,60 @@ Blockly.Blocks['get_digital_input_value'] = {
 Blockly.Java['get_digital_input_value'] = function(block) {
   var variable_name = Blockly.Java.variableDB_.getName(block.getFieldValue('NAME'), Blockly.Variables.NAME_TYPE);
   
-  //@todo make sure wariable_name has been declared
+  //@todo make sure variable_name has been declared
   var code = variable_name+'.get()';
+  return code;
+};
+
+// Digital Output
+
+Blockly.Blocks['digital_output'] = {
+  init: function() {
+    this.setHelpUrl('http://www.example.com/');
+    this.setTooltip('');
+    this.setColour(120);
+    this.appendDummyInput()
+        .appendField("Declare Digital Output")
+        .appendField(new Blockly.TypedFieldVariable("Dout1", "DigitalOutput"), "NAME");
+    this.appendDummyInput()
+        .appendField("on port")
+        .appendField(new Blockly.FieldTextInput("1", EasyJ.Checker.DIGITAL_PORT), "PORT");
+    // this.appendValueInput("PORT")
+    //     .setCheck("Number")
+    //     .appendField("on Port");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true, 'declare');
+    this.setNextStatement(true, 'declare');
+  }
+};
+Blockly.Java['digital_output'] = function(block) {
+  var value_port = block.getFieldValue('PORT');
+  var variable_name = Blockly.Java.variableDB_.getName(block.getFieldValue('NAME'), Blockly.Variables.NAME_TYPE);
+  
+  Blockly.Java.addImport("import edu.wpi.first.wpilibj.DigitalOutput;");
+  var code = 'DigitalInput '+variable_name+' = new DigitalInput('+value_port+');';
+  return code;
+};
+
+Blockly.Blocks['set_digital_output'] = {
+  init: function() {
+    this.setHelpUrl('http://www.example.com/');
+    this.setTooltip('');
+    this.setColour(120);
+    this.appendDummyInput()
+        .appendField("Set value")
+        .appendField(new Blockly.TypedFieldVariable("Dout1", "DigitalOutput"), "NAME");
+    this.appendDummyInput()
+        .appendField("to")
+        .appendField(new Blockly.FieldDropdown([["True", "True"], ["False", "False"]]), "VALUE");
+    this.setInputsInline(true);
+    this.setOutput(true, "Number");
+  }
+};
+Blockly.Java['set_digital_output'] = function(block) {
+  var variable_name = Blockly.Java.variableDB_.getName(block.getFieldValue('NAME'), Blockly.Variables.NAME_TYPE);
+  var dropdown_value = block.getFieldValue('VALUE');
+  //@todo make sure variable_name has been declared
+  var code = variable_name+'.set('+dropdown_value+')';
   return code;
 };
