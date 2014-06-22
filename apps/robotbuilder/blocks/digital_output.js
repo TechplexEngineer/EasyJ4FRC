de86c22ea1bec3a392375eae03044624323f4973
Blake Bourque <Techplex.Engineer@gmail.com>
2014-06-22 12:53:14 +0000

    Split the digital blocks into input and output iles

--- apps/robotbuilder/blocks/digital_output.js
+++ apps/robotbuilder/blocks/digital_output.js
@@ -1,76 +1,3 @@
-// Digital
-
-Blockly.Blocks['declare_digital_input'] = {
-  init: function() {
-    this.setHelpUrl('');
-    this.setTooltip('Create a digital input which allows reading of a digital signal.');
-    this.setColour(120);
-    this.appendDummyInput()
-        .appendField("Declare Digital Input")
-        .appendField(new Blockly.TypedFieldVariable("Din1", "DigitalInput", true), "NAME");
-    this.appendDummyInput()
-        .appendField("on port")
-        .appendField(new Blockly.FieldTextInput("1", EasyJ.Checker.DIGITAL_PORT), "PORT");
-    // this.appendValueInput("PORT")
-    //     .setCheck("Number")
-    //     .appendField("on Port");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, 'declare');
-    this.setNextStatement(true, 'declare');
-  },
-  onchange: EasyJ.Checker.EnsureNotTop_Init,
-  renameVar: function(oldName, newName) {
-    if (Blockly.Names.equals(oldName, this.getFieldValue('NAME'))) {
-      this.setFieldValue(newName, 'NAME');
-    }
-  }
-};
-Blockly.Java['declare_digital_input'] = function(block) {
-  var value_port = block.getFieldValue('PORT');
-  var variable_name = Blockly.Java.variableDB_.getName(block.getFieldValue('NAME'), Blockly.Variables.NAME_TYPE);
-
-  Blockly.Java.addImport("import edu.wpi.first.wpilibj.DigitalInput;");
-  var code = 'DigitalInput '+variable_name+' = new DigitalInput('+value_port+');';
-  return code;
-};
-
-Blockly.Blocks['get_digital_input_value'] = {
-  init: function() {
-    this.setHelpUrl('');
-    this.setTooltip('Get the current value of the digital input.');
-    this.setColour(120);
-    this.appendDummyInput()
-        .appendField("Get value")
-        .appendField(new Blockly.TypedFieldVariable("Din1", "DigitalInput"), "NAME");
-    this.setInputsInline(true);
-    this.setOutput(true, "Number");
-	this.setDependsOn("declare_digital_input");
-  },
-  onchange: function(evt) {
-   if (!this.workspace || this.isInFlyout) {
-      // Block has been deleted, or is in flyout
-      return;
-    }
-    var block = this;
-    this.setWarningText(EasyJ.Checker.PickWarning(block, [EasyJ.Checker.EnsureVariablesExist, EasyJ.Checker.EnsureNotOrphaned]));
-
-  },
-  renameVar: function(oldName, newName) {
-    if (Blockly.Names.equals(oldName, this.getFieldValue('NAME'))) {
-      this.setFieldValue(newName, 'NAME');
-    }
-  }
-};
-Blockly.Java['get_digital_input_value'] = function(block) {
-  var variable_name = Blockly.Java.variableDB_.getName(block.getFieldValue('NAME'), Blockly.Variables.NAME_TYPE);
-
-  //@todo make sure variable_name has been declared
-  var code = variable_name+'.get()';
-  return code;
-};
-
-// Digital Output
-
 Blockly.Blocks['declare_digital_output'] = {
   init: function() {
     this.setHelpUrl('http://www.example.com/');
@@ -99,7 +26,7 @@ Blockly.Blocks['declare_digital_output'] = {
 Blockly.Java['declare_digital_output'] = function(block) {
   var value_port = block.getFieldValue('PORT');
   var variable_name = Blockly.Java.variableDB_.getName(block.getFieldValue('NAME'), Blockly.Variables.NAME_TYPE);
-  
+
   Blockly.Java.addImport("import edu.wpi.first.wpilibj.DigitalOutput;");
   var code = 'DigitalInput '+variable_name+' = new DigitalInput('+value_port+');';
   return code;
@@ -122,7 +49,7 @@ Blockly.Blocks['set_digital_output'] = {
     this.setInputsInline(true);
     this.setPreviousStatement(true, 'statement');
     this.setNextStatement(true, 'statement');
-	this.setDependsOn("set_digital_output");
+	this.setDependsOn("declare_digital_output");
   },
   onchange: function(evt) {
    if (!this.workspace || this.isInFlyout) {
