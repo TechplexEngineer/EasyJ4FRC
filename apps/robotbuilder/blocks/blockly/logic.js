335d593096bd16288fd2399347bfc3298d8199de
Blake Bourque <Techplex.Engineer@gmail.com>
2014-06-24 23:49:42 +0000

    These blocks are needed for the If mutator

--- apps/robotbuilder/blocks/blockly/logic.js
+++ apps/robotbuilder/blocks/blockly/logic.js
@@ -218,51 +218,53 @@ Blockly.Blocks['controls_if'] = {
   }
 };
 
-// Blockly.Blocks['controls_if_if'] = {
-//   /**
-//    * Mutator block for if container.
-//    * @this Blockly.Block
-//    */
-//   init: function() {
-//     this.setColour(210);
-//     this.appendDummyInput()
-//         .appendField(Blockly.Msg.CONTROLS_IF_IF_TITLE_IF);
-//     this.appendStatementInput('STACK');
-//     this.setTooltip(Blockly.Msg.CONTROLS_IF_IF_TOOLTIP);
-//     this.contextMenu = false;
-//   }
-// };
 
-// Blockly.Blocks['controls_if_elseif'] = {
-//   /**
-//    * Mutator bolck for else-if condition.
-//    * @this Blockly.Block
-//    */
-//   init: function() {
-//     this.setColour(210);
-//     this.appendDummyInput()
-//         .appendField(Blockly.Msg.CONTROLS_IF_ELSEIF_TITLE_ELSEIF);
-//     this.setPreviousStatement(true);
-//     this.setNextStatement(true);
-//     this.setTooltip(Blockly.Msg.CONTROLS_IF_ELSEIF_TOOLTIP);
-//     this.contextMenu = false;
-//   }
-// };
-
-// Blockly.Blocks['controls_if_else'] = {
-//   /**
-//    * Mutator block for else condition.
-//    * @this Blockly.Block
-//    */
-//   init: function() {
-//     this.setColour(210);
-//     this.appendDummyInput()
-//         .appendField(Blockly.Msg.CONTROLS_IF_ELSE_TITLE_ELSE);
-//     this.setPreviousStatement(true);
-//     this.setTooltip(Blockly.Msg.CONTROLS_IF_ELSE_TOOLTIP);
-//     this.contextMenu = false;
-//   }
-// };
+//For mutator
+Blockly.Blocks['controls_if_if'] = {
+  /**
+   * Mutator block for if container.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.setColour(210);
+    this.appendDummyInput()
+        .appendField(Blockly.Msg.CONTROLS_IF_IF_TITLE_IF);
+    this.appendStatementInput('STACK');
+    this.setTooltip(Blockly.Msg.CONTROLS_IF_IF_TOOLTIP);
+    this.contextMenu = false;
+  }
+};
+//For mutator
+Blockly.Blocks['controls_if_elseif'] = {
+  /**
+   * Mutator bolck for else-if condition.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.setColour(210);
+    this.appendDummyInput()
+        .appendField(Blockly.Msg.CONTROLS_IF_ELSEIF_TITLE_ELSEIF);
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setTooltip(Blockly.Msg.CONTROLS_IF_ELSEIF_TOOLTIP);
+    this.contextMenu = false;
+  }
+};
+//For mutator
+Blockly.Blocks['controls_if_else'] = {
+  /**
+   * Mutator block for else condition.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.setColour(210);
+    this.appendDummyInput()
+        .appendField(Blockly.Msg.CONTROLS_IF_ELSE_TITLE_ELSE);
+    this.setPreviousStatement(true);
+    this.setTooltip(Blockly.Msg.CONTROLS_IF_ELSE_TOOLTIP);
+    this.contextMenu = false;
+  }
+};
 
 Blockly.Blocks['logic_compare'] = {
   /**
